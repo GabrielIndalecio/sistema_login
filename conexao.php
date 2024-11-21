c5cfa3132ec56edec6923e9421044aa9187cfe21
Gabriel Indalecio <xxgabrieelindalecio8@gmail.com>
2024-11-21 14:55:28 -0300

    Criando o PHP
    PHP Conexao

---
+++ conexao.php
@@ -0,0 +1,22 @@
+<?php
+/* COnexao com bd usando PDO : PDO permite acessar qualquer bancode de dados. */
+
+$host = 'localhost';
+$dbname = 't57_login';
+$usuario = 'root';
+$senha = '';
+
+
+$conn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+try{
+    // Cria a conexão
+    $conexao = new PDO($conn,$usuario,$senha);
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+    
+
+}catch(PDOException $e){
+    die("Erro de conexao" .$e->getMessage());
+}
