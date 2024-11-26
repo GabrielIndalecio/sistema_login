2407379a237c0ec7bbc746b3264be6dcaa6b314d
Gabriel Indalecio <xxgabrieelindalecio8@gmail.com>
2024-11-26 16:15:22 -0300

    Tela painel.php
    Tela painel.php

--- painel/painel.php
+++ painel/painel.php
@@ -1,14 +1,33 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../image/favicon.png" type="image/x-png">
     <link rel="stylesheet" href="painel.css">
     <title>Painel</title>
 </head>
 <body>
-    <div class="Principal">
-        <img src="https://icons.iconarchive.com/icons/iconarchive/dogecoin-to-the-moon/128/Dogecoin-on-Moon-icon.png"> 
+    <div class="Cabecario">
+        <h1>L.G.G.D </h1>
+        <a href="painel.php">
+            <img src="https://icons.iconarchive.com/icons/iconarchive/dogecoin-to-the-moon/128/Dogecoin-on-Moon-icon.png"> 
+        </a>
+        <form action="">
+            <button type="submit" class="botaocategoria">Categorias</button>
+        </form>
+        <form action="logout.php">
+            <button type="submit" class="botaosair">Sair</button>
+        </form>
+    </div>
+    <div class="meio">
+        <div class="shampoo">
+            <a href="../image/pngimg.com - shampoo_PNG99887.png">
+                <img src="../image/pngimg.com - shampoo_PNG99887.png" alt="shampoo">
+                <label class="labelsha">Shampoo do Cristano Ronaldo</label>
+            </a>
+        </div>
     </div>
 </body>
 </html>
